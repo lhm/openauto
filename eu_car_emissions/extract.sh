6d6f7c2194215b457c3a39c9c5c6489fe5f3a019
Lars Henrik Mai <lars.mai@kontinui.de>
2019-10-01 14:47:57 +0200

    Move shell scripts to bin directory

--- eu_car_emissions/extract.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-curl -O http://ftp.eea.europa.eu/www/co2/CO2_passenger_cars_v17_csv.zip
-unzip CO2_passenger_cars_v17_csv.zip
--- eu_car_emissions/shell.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --volume="`pwd`:/app" --workdir '/app' daskdev/dask:2.3.0 python -ic 'from transform import *'
